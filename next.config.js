8165be6f9ed605dbe4c2d7ef54b98a95f204132d
alex <aleksa.kumric@gmail.com>
2023-10-19 20:03:17 +0200

    initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextTranslate = require("next-translate-plugin")
+
+
+const nextConfig = {
+    reactStrictMode: true,
+}
+
+module.exports = nextTranslate({
+    ...nextConfig
+})
