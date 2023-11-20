c7371fba9cf774f14acdf8e1fa1cf35c19473723
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2023-11-20 12:07:09 +0100

    minor text and styling changes

--- app/details/Details.tsx
+++ app/details/Details.tsx
@@ -14,12 +14,8 @@ const Details = () => {
         </h1>
         <p className="text-grey-600 text-xl my-2 w-7/12">
           Please do not look at code as if it where completed <br />
-          things to be done as of currently implemented features: correct all
-          types, wrapp inputs, translate everithing, delete watch, make radio in
-          dynamic component and accept data as props, make utils folder and
-          helpers.ts files for functions, make services folder and service for
-          every component that keeps all functions and types, make error dynamic
-          component (one error component for every field)
+          Completed features: Login Page and Register Page. Results are logged
+          to console.
         </p>
       </div>
       {/* <div className={styles.testClass}>{t("header-title")}</div> */}
